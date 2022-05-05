1e9fdccdb83a6de97b7757b7d4ad55cbdac2326b
GCaringi <giannicaringi94@gmail.com>
2022-05-05 12:40:16 +0200

    Add alternative solution

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,14 +1,38 @@
+// let newArray = [];
+// let sum = 0;
+// let num;
+
+// do{
+//     do{
+//         num = Number(prompt("Inserisci un numero"));
+//     }while (isNaN(num));
+//     sum += num;
+//     newArray.push(num);
+// }while (sum < 50);
+
+// console.log(sum);
+// console.log(newArray);
+
+
+// Soluzione dove non eccede la somma di 50;
+
 let newArray = [];
 let sum = 0;
 let num;
 
+
 do{
+    num = Number(prompt("Inserisci un numero"));
+}while (isNaN(num));
+sum += num;
+
+while(sum < 50){
+    newArray.push(num);
     do{
         num = Number(prompt("Inserisci un numero"));
     }while (isNaN(num));
     sum += num;
-    newArray.push(num);
-}while (sum < 50);
+}
 
 console.log(sum);
 console.log(newArray);
